e2cf68c653d98e76d43aa7f72a657182728c78b5
Gauri <gauri.gandhii2004@gmail.com>
2025-10-31 01:21:57 +0530

    docker

--- TaskManagerBackend/TaskManagerBackend/obj/Debug/net8.0/TaskManagerBackend.AssemblyInfo.cs
+++ TaskManagerBackend/TaskManagerBackend/obj/Debug/net8.0/TaskManagerBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c48483dd882a37a7b67af3f4e66e625218756e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
